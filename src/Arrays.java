d41c029f7668e7552abacb82e6cd661f91474335
szewc <szewc1@gmail.com>
2013-08-25 20:22:42 +0200

    arrays

---
+++ src/Arrays.java
@@ -0,0 +1,31 @@
+public class Arrays {
+	public static void main(String[] args) {
+		
+		/* int[] values = new int[4];
+		values[0] = 10; // values' index is starting at 0
+		values[1] = 20;
+		values[2] = 30;
+		values[3] = 40;
+		
+		for (int i = 0; i < values.length; i++) {
+			System.out.println(values[i]);
+		}
+		
+		int[] numbers = {6,7,8,9};
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.println(numbers[i]);
+		}
+		*/
+		
+		int test[] = new int[9];
+		test[0] = 1;
+		int k = 0;
+		
+		for ( test[0] = 1; test[8] < test.length; test[k]++) {
+		}
+		
+		for (int i = 0; i < test.length; i++) {
+			System.out.println(test[i]);
+		}
+	}
+}
